8380143c15aede03e52e202f2fc4f444707cd36f
RubelBiswasCS <rubelbiswas@gmail.com>
2021-12-11 18:56:21 +0600

    added new componet and new packages

---
+++ src/components/Main.jsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+
+const Main = () => {
+
+    return (
+        <React.Fragment>
+            <Header/>
+            <Footer/>
+        </React.Fragment>
+    );
+};
+export default Main;
