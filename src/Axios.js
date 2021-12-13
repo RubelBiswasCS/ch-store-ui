323a41bc90ac4c7ac8f6233c3b7301033e6abb8b
RubelBiswasCS <rubelbiswas@gmail.com>
2021-12-14 00:04:20 +0600

    modified axios

--- src/Axios.js
+++ src/Axios.js
@@ -6,6 +6,9 @@ const axiosInstance = axios.create({
     baseURL : baseUrl,
     timeout : 5000,
     headers : {
+        Authorization: localStorage.getItem('access_token')
+			? 'Bearer ' + localStorage.getItem('access_token')
+			: null,
         'Content-Type': 'application/json',
 		accept: 'application/json',
     },
