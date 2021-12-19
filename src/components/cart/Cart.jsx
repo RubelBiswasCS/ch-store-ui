2f8ce38a3b4b07c10dad4e56de42bda02198b664
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-12-20 00:47:33 +0600

    modified cart style

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Popper from '@mui/material/Popper';
 import Typography from '@mui/material/Typography';
 import Fade from '@mui/material/Fade';
@@ -22,10 +23,10 @@ export default function Cart(props) {
       <Popper open={open} anchorEl={anchorEl} placement={'bottom-end'} transition>
         {({ TransitionProps }) => (
           <Fade {...TransitionProps} timeout={350}>
-            <Paper sx={{width:{sm:"90vw",md:'15vw'}}}>
-              <Typography sx={{ p: 2 }}>cart</Typography>
+            <Paper sx={{width:{sm:"90vw",md:'20vw'}}}>
+              <Typography sx={{ p: 2,display:"flex",justifyContent:"center",borderBottom:"1px solid blue" }}>Cart</Typography>
               {cartItems.map( item => (
-                <Grid container sp={2} sx={{display:'flex',fontSize:'.5em',borderBottom:'1px solid black',justifyContent:'center',alignItems:'center',gap:'2px',padding:'2px'}} className={'cartItem'} key={item.name}>
+                <Grid container sp={2} sx={{display:'flex',fontSize:'.5em',borderBottom:'1px solid black',justifyContent:'center',alignItems:'center',gap:'5px',padding:'2px'}} className={'cartItem'} key={item.name}>
                   <Grid item sx={{width:"20%",justifyContent:'center',padding:'2px'}}>
                     <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column',padding:'0px' }} >
                       <CardMedia
@@ -40,17 +41,20 @@ export default function Cart(props) {
                       />
                     </Card>
                   </Grid>
-                  <Grid item sx={{display:'flex',flexDirection:'column',width:'60%',justifyContent:'center',border:"0px solid red"}}>
+                  <Grid item sx={{display:'flex',flexDirection:'column',width:'55%',justifyContent:'center',border:"0px solid red"}}>
                     <Box>
                       <Typography sx={{fontSize:'1em'}}>{item.name}</Typography>
                     </Box>
                     <Box  sx={{display:'flex',justifyContent:'center'}}>
-                      <Typography sx={{fontSize:'1em'}}>{item.quantity}</Typography>
+                      <Button sx={{fontSize:"1.2em",maxWidth:"20%",maxHeight:'10%',padding:0,backgroundColor:'pink'}}>-</Button>
+                      <Box sx={{width:'30%',display:"flex",justifyContent:"center",alignItems:'center'}}><Typography sx={{fontSize:'1em',textAlign:'center',width:'100%'}}>{item.quantity}</Typography></Box>
+                      <Button sx={{fontSize:"1.2em",maxWidth:"20%",maxHeight:'10%',padding:0,backgroundColor:'lightblue'}}>+</Button>
                     </Box>
                   </Grid>
-                  <Grid item sx={{display:'flex',width:'15%',justifyContent:'center',alignItems:'center',flexDirection:'column',border:"0px solid red"}}>
-                    <Box sx={{display:'flex',justifyContent:'center',alignItems:'center',background:'inherit'}}>
+                  <Grid item sx={{display:'flex',width:'20%',justifyContent:'center',alignItems:'center',flexDirection:'column',border:"0px solid red"}}>
+                    <Box sx={{display:'flex', flexDirection: 'column', justifyContent:'center',alignItems:'center',padding:"30%",background:'inherit'}}>
                       <Typography sx={{fontSize:'1em'}}>{item.unit_price}</Typography>
+                      <Button sx={{fontSize:".7em",maxWidth:"40%"}}>Remove</Button>
                     </Box>
                     <Box>
 
