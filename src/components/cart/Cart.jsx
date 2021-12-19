f1e7d8027cc9f3a0f040ee1f9d0816c70eeaf506
RubelBiswasCS <rubelbiswas36@gmail.com>
2021-12-20 00:08:17 +0600

    modified cart

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -25,14 +25,14 @@ export default function Cart(props) {
             <Paper sx={{width:{sm:"90vw",md:'15vw'}}}>
               <Typography sx={{ p: 2 }}>cart</Typography>
               {cartItems.map( item => (
-                <Grid container sx={{display:'flex',fontSize:'.5em'}} className={'cartItem'} key={item.name}>
-                  <Grid item>
-                    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column' }} >
+                <Grid container sp={2} sx={{display:'flex',fontSize:'.5em',borderBottom:'1px solid black',justifyContent:'center',alignItems:'center',gap:'2px',padding:'2px'}} className={'cartItem'} key={item.name}>
+                  <Grid item sx={{width:"20%",justifyContent:'center',padding:'2px'}}>
+                    <Card sx={{ height: '100%', display: 'flex', flexDirection: 'column',padding:'0px' }} >
                       <CardMedia
                       component="img"
                       sx={{
                           // 16:9
-                          maxHeight:'70%',
+                          maxHeight:'100%',
                           padding:'5%',
                       }}
                       image="https://source.unsplash.com/random"
@@ -40,17 +40,17 @@ export default function Cart(props) {
                       />
                     </Card>
                   </Grid>
-                  <Grid item sx={{display:'flex',flexDirection:'column'}}>
+                  <Grid item sx={{display:'flex',flexDirection:'column',width:'60%',justifyContent:'center',border:"0px solid red"}}>
                     <Box>
                       <Typography sx={{fontSize:'1em'}}>{item.name}</Typography>
                     </Box>
                     <Box  sx={{display:'flex',justifyContent:'center'}}>
-                      <Typography>{item.quantity}</Typography>
+                      <Typography sx={{fontSize:'1em'}}>{item.quantity}</Typography>
                     </Box>
                   </Grid>
-                  <Grid item sx={{display:'flex',justifyContent:'center',flexDirection:'column'}}>
-                    <Box sx={{display:'flex',justifyContent:'center'}}>
-                      <Typography>{item.unit_price}</Typography>
+                  <Grid item sx={{display:'flex',width:'15%',justifyContent:'center',alignItems:'center',flexDirection:'column',border:"0px solid red"}}>
+                    <Box sx={{display:'flex',justifyContent:'center',alignItems:'center',background:'inherit'}}>
+                      <Typography sx={{fontSize:'1em'}}>{item.unit_price}</Typography>
                     </Box>
                     <Box>
 
